9a91cd85929abd32c7c5d5c14f2eeb76e68d75a6
PauloEduardoCardoso <pauloeducardoso@gmail.com>
2020-02-17 22:35:45 +0000

    travis warning

--- tests/testthat/test-vrings.R
+++ tests/testthat/test-vrings.R
@@ -1,12 +1,9 @@
-context('vrings')
-test_that("correct object size from vrings", {
+test_that(context("Object size from vrings"), {
   # fail to resolve object dimension
   library(genestdwp)
   library(sf)
   library(tidyverse)
-  # Vector of distances
   dist = c(10, 20, 30, 40, 50)
-  # Spatial join among visibility map and rings
   rings <- viring(x=visib, d = dist)
-  expect_equal(dim(rings), c(1965,7))
+  expect_equal(dim(rings), c(1965, 7))
 })
