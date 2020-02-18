875fcc0ac36050ad45d4517bb26191826cc36415
PauloEduardoCardoso <pauloeducardoso@gmail.com>
2020-02-18 00:27:39 +0000

    dwp test error

--- tests/testthat/test-dwp.R
+++ tests/testthat/test-dwp.R
@@ -1,11 +1,9 @@
+rings <- viring(x=visib, d = dist)
 test_that(context("Object size from dwp"), {
-  # fail to resolve object dimension
   library(genestdwp)
   library(sf)
   library(tidyverse)
   dist = c(10, 20, 30, 40, 50)
-  rings <- viring(x=visib, d = dist)
   df <- dwp(vr = rings, pt = pto_carcass)
   expect_equal(dim(df), c(37, 2))
-  expect_warning(nrow(filter(cr, visib == 0)) != 0)
 })
